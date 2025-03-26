abfdb4d57cbf2c2b09607a51fca66610cccd4d95
adiganga2002 <38322305+adiganga2002@users.noreply.github.com>
2025-03-26 16:58:29 +0200

    prelounch catch email error

--- EmailPlugin.js
+++ EmailPlugin.js
@@ -1,13 +1,14 @@
 const sgMail = require('@sendgrid/mail');
-async function EmailPlugin(){
+
+async function EmailPlugin() {
     let self = {};
-    if(!process.env.SENDGRID_API_KEY){
+    if (!process.env.SENDGRID_API_KEY) {
         console.error("SentGRID API key missing. using default key");
         //must set sender email support@outfinitygift.com in env, for testing
         process.env.SENDGRID_API_KEY = "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";
     }
     sgMail.setApiKey(process.env.SENDGRID_API_KEY);
-    self.sendEmail = async function(to, from, subject, text, html){
+    self.sendEmail = async function (to, from, subject, text, html) {
         const msg = {
             to: to,
             from: from,
@@ -15,16 +16,25 @@ async function EmailPlugin(){
             text: text,
             html: html
         };
-        await sgMail.send(msg);
+        try {
+            await sgMail.send(msg);
+        } catch (e) {
+            console.log(e);
+        }
+
     }
-    self.sendFromTemplate = async function(to, from, templateId, dynamicTemplateData){
+    self.sendFromTemplate = async function (to, from, templateId, dynamicTemplateData) {
         const msg = {
             to: to,
             from: from,
             templateId: templateId,
             dynamicTemplateData: dynamicTemplateData,
         };
-        await sgMail.send(msg);
+        try {
+            await sgMail.send(msg);
+        } catch (e) {
+            console.log(e);
+        }
     }
     return self;
 }
@@ -33,13 +43,13 @@ let singletonInstance = undefined;
 
 module.exports = {
     getInstance: async function () {
-        if(!singletonInstance){
+        if (!singletonInstance) {
             singletonInstance = await EmailPlugin();
         }
         return singletonInstance;
     },
-    getAllow: function(){
-        return async function(globalUserId, email, command, ...args){
+    getAllow: function () {
+        return async function (globalUserId, email, command, ...args) {
             return true;
         }
     }
